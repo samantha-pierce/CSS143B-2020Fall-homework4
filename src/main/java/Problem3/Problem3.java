d069cc35bb7c11c87a9de3ed74617d377dd80464
Samantha-Pierce <spapierce@gmail.com>
2020-11-03 07:59:24 -0800

    problem3 completed with tests passing

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -7,6 +7,12 @@ import java.io.PrintStream;
 public class Problem3 {
     public static void printListInReverse(ListNode list, PrintStream print) {
         // homework
-        
+        if (list == null) {
+            return;
+        }
+        if (list.next != null) {
+            printListInReverse(list.next, print);
+        }
+        print.println(list.val);
     }
 }
